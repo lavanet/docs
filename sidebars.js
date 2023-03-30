ef90391d8b82bee383df6e56751310ab0225d0c2
Sean Katz <sean@lavanet.xyz>
2023-03-30 11:46:19 +0300

    Add Freeze command (#116)

--- sidebars.js
+++ sidebars.js
@@ -78,6 +78,7 @@ const sidebars = {
           },
           items: [
             'provider/provider-setup',
+            'provider/provider-features'
           ]
         },    
         'lava-blockchain/faucet',
