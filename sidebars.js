02923dc49b9c009a42d53740f8f4aa4e248996be
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2023-12-19 00:07:01 -0600

    adjust sidebar navigation

--- sidebars.js
+++ sidebars.js
@@ -14,8 +14,216 @@ const sidebars = {
       collapsed: true,
       items: [
         'intro/about',
+        'intro/explorers',
         'intro/key-terms',
-        'intro/faq',
+        'intro/faq'
+      ]
+    },
+    {
+      type: 'category', 
+      label: 'Chains ⛓️',
+      link: {
+        type: 'doc',
+        id: 'chains/chains'
+      },
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: 'category',
+          label: '🔗 Avalanche',
+          collapsible: true,
+          collapsed: true,
+          description: 'AVAX, AVAXT',
+          link: {
+            type: 'doc',
+            id: 'chains/avalanche-chain/avalanche',
+          },
+          items: [
+            'chains/avalanche-chain/avalanche-dev',
+            'chains/avalanche-chain/avalanche-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Axelar',
+          collapsible: true,
+          collapsed: true,
+          description: 'AXELAR, AXELART',
+          link: {
+            type: 'doc',
+            id: 'chains/axelar-chain/axelar'
+          },
+          items: [
+            'chains/axelar-chain/axelar-dev',
+            'chains/axelar-chain/axelar-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Celo',
+          collapsible: true,
+          collapsed: true,
+          description: 'CELO, ALFAJORES',
+          link: {
+            type: 'doc',
+            id: 'chains/celo-chain/celo'
+          },
+          items: [
+            'chains/celo-chain/celo-dev',
+            'chains/celo-chain/celo-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 CosmosHub',
+          collapsible: true,
+          collapsed: true,
+          description: 'COS5, COS5T',
+          link: {
+            type: 'doc',
+            id: 'chains/cosmoshub-chain/cosmoshub'
+          },
+          items: [
+            'chains/cosmoshub-chain/cosmoshub-dev',
+            'chains/cosmoshub-chain/cosmoshub-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Evmos',
+          collapsible: true,
+          collapsed: true,
+          description: 'EVMOS, EVMOST',
+          link: {
+            type: 'doc',
+            id: 'chains/evmos-chain/evmos'
+          },
+          items: [
+            'chains/evmos-chain/evmos-dev',
+            'chains/evmos-chain/evmos-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Ethereum',
+          collapsible: true,
+          collapsed: true,
+          description: 'ETH1, GTH1',
+          link: {
+            type: 'doc',
+            id: 'chains/ethereum-chain/ethereum'
+          },
+          items: [
+            'chains/ethereum-chain/ethereum-dev',
+            'chains/ethereum-chain/ethereum-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Filecoin',
+          collapsible: true,
+          collapsed: true,
+          description: 'FVM',
+          link: {
+            type: 'doc',
+            id: 'chains/filecoin-chain/filecoin'
+          },
+          items: [
+            'chains/filecoin-chain/filecoin-dev',
+            'chains/filecoin-chain/filecoin-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Juno',
+          collapsible: true,
+          collapsed: true,
+          description: 'JUN1, JUNT1',
+          link: {
+            type: 'doc',
+            id: 'chains/juno-chain/juno'
+          },
+          items: [
+            'chains/juno-chain/juno-dev',
+            'chains/juno-chain/juno-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Lava',
+          description: 'LAV1',
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: 'doc',
+            id: 'chains/lava-chain/lava'
+          },
+          items: [
+            'chains/lava-chain/lava-dev',
+            'chains/lava-chain/lava-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 NEAR',
+          description: 'NEAR, NEART',
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: 'doc',
+            id: 'chains/near-chain/near'
+          },
+          items: [
+            'chains/near-chain/near-dev',
+            'chains/near-chain/near-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Optimism',
+          collapsible: true,
+          collapsed: true,
+          description: 'OPTM, OPTMT',
+          link: {
+            type: 'doc',
+            id: 'chains/optimism-chain/optimism'
+          },
+          items: [
+            'chains/optimism-chain/optimism-dev',
+            'chains/optimism-chain/optimism-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Solana',
+          collapsible: true,
+          collapsed: true,
+          description: 'SOL, SOLT',
+          link: {
+            type: 'doc',
+            id: 'chains/solana-chain/solana'
+          },
+          items: [
+            'chains/solana-chain/solana-dev',
+            'chains/solana-chain/solana-node'
+          ]
+        },
+        {
+          type: 'category',
+          label: '🔗 Starknet',
+          collapsible: true,
+          collapsed: true,
+          description: 'STRK, STRKT',
+          link: {
+            type: 'doc',
+            id: 'chains/starknet-chain/starknet'
+          },
+          items: [
+            'chains/starknet-chain/starknet-dev',
+            'chains/starknet-chain/starknet-node'
+          ]
+        }
       ]
     },
     {
@@ -71,16 +279,14 @@ const sidebars = {
             collapsible: true,
             collapsed: true,
             link: {
-              type: 'doc',
-              id: 'developers/sdk/access-sdk'
+              type: 'generated-index'
             },
             items: [
-              'developers/sdk/sdk-multichain-example',
-              'developers/sdk/lava-sdk-gallery',
-              'developers/sdk/lava-bazaar',
+              'developers/sdk/examples/sdk-multichain-example',
+              'developers/sdk/examples/lava-sdk-gallery',
+              'developers/sdk/examples/lava-bazaar',
             ]
           },
-
         ]
       },
       {
@@ -118,236 +324,14 @@ const sidebars = {
     },
     {
       type: 'category', 
-      label: 'Chains ⛓️',
-      link: {
-        type: 'doc',
-        id: 'chains/chains'
-      },
-      collapsible: true,
-      collapsed: true,
-      items: [
-        {
-          type: 'category',
-          label: 'Avalanche',
-          collapsible: true,
-          collapsed: true,
-          description: 'AVAX, AVAXT',
-          link: {
-            type: 'doc',
-            id: 'chains/avalanche-chain/avalanche',
-          },
-          items: [
-            'chains/avalanche-chain/avalanche-dev',
-            'chains/avalanche-chain/avalanche-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Axelar',
-          collapsible: true,
-          collapsed: true,
-          description: 'AXELAR, AXELART',
-          link: {
-            type: 'doc',
-            id: 'chains/axelar-chain/axelar'
-          },
-          items: [
-            'chains/axelar-chain/axelar-dev',
-            'chains/axelar-chain/axelar-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Celo',
-          collapsible: true,
-          collapsed: true,
-          description: 'CELO, ALFAJORES',
-          link: {
-            type: 'doc',
-            id: 'chains/celo-chain/celo'
-          },
-          items: [
-            'chains/celo-chain/celo-dev',
-            'chains/celo-chain/celo-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'CosmosHub',
-          collapsible: true,
-          collapsed: true,
-          description: 'COS5, COS5T',
-          link: {
-            type: 'doc',
-            id: 'chains/cosmoshub-chain/cosmoshub'
-          },
-          items: [
-            'chains/cosmoshub-chain/cosmoshub-dev',
-            'chains/cosmoshub-chain/cosmoshub-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Evmos',
-          collapsible: true,
-          collapsed: true,
-          description: 'EVMOS, EVMOST',
-          link: {
-            type: 'doc',
-            id: 'chains/evmos-chain/evmos'
-          },
-          items: [
-            'chains/evmos-chain/evmos-dev',
-            'chains/evmos-chain/evmos-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Ethereum',
-          collapsible: true,
-          collapsed: true,
-          description: 'ETH1, GTH1',
-          link: {
-            type: 'doc',
-            id: 'chains/ethereum-chain/ethereum'
-          },
-          items: [
-            'chains/ethereum-chain/ethereum-dev',
-            'chains/ethereum-chain/ethereum-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Filecoin',
-          collapsible: true,
-          collapsed: true,
-          description: 'FVM',
-          link: {
-            type: 'doc',
-            id: 'chains/filecoin-chain/filecoin'
-          },
-          items: [
-            'chains/filecoin-chain/filecoin-dev',
-            'chains/filecoin-chain/filecoin-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Juno',
-          collapsible: true,
-          collapsed: true,
-          description: 'JUN1, JUNT1',
-          link: {
-            type: 'doc',
-            id: 'chains/juno-chain/juno'
-          },
-          items: [
-            'chains/juno-chain/juno-dev',
-            'chains/juno-chain/juno-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Lava',
-          description: 'LAV1',
-          collapsible: true,
-          collapsed: true,
-          link: {
-            type: 'doc',
-            id: 'chains/lava-chain/lava'
-          },
-          items: [
-            'chains/lava-chain/lava-dev',
-            'chains/lava-chain/lava-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'NEAR',
-          description: 'NEAR, NEART',
-          collapsible: true,
-          collapsed: true,
-          link: {
-            type: 'doc',
-            id: 'chains/near-chain/near'
-          },
-          items: [
-            'chains/near-chain/near-dev',
-            'chains/near-chain/near-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Optimism',
-          collapsible: true,
-          collapsed: true,
-          description: 'OPTM, OPTMT',
-          link: {
-            type: 'doc',
-            id: 'chains/optimism-chain/optimism'
-          },
-          items: [
-            'chains/optimism-chain/optimism-dev',
-            'chains/optimism-chain/optimism-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Solana',
-          collapsible: true,
-          collapsed: true,
-          description: 'SOL, SOLT',
-          link: {
-            type: 'doc',
-            id: 'chains/solana-chain/solana'
-          },
-          items: [
-            'chains/solana-chain/solana-dev',
-            'chains/solana-chain/solana-node'
-          ]
-        },
-        {
-          type: 'category',
-          label: 'Starknet',
-          collapsible: true,
-          collapsed: true,
-          description: 'STRK, STRKT',
-          link: {
-            type: 'doc',
-            id: 'chains/starknet-chain/starknet'
-          },
-          items: [
-            'chains/starknet-chain/starknet-dev',
-            'chains/starknet-chain/starknet-node'
-          ]
-        }
-      ]
-    },
-    {
-      type: 'category', 
-      label: 'Web3 APIs ⚡️',
+      label: 'Validators ⚖️',
       collapsible: true,
       collapsed: true,
       link: {
         type: 'doc',
-        id: 'access-intro'
+        id: 'validator/validator'
       },
       items: [
-        'public-rpc',
-      ],
-    },
-    {
-      type: 'category', 
-      label: 'Lava Protocol 🌋',
-      collapsible: true,
-      collapsed: true,
-      link: {
-        type: 'doc',
-        id: 'power-lava-intro'
-      },
-      items: [
-        'lava-blockchain/install-lava',
-        'lava-blockchain/hardware-requirements',
         {
           type: 'category', 
           label: 'Join testnet',
@@ -362,59 +346,73 @@ const sidebars = {
             'lava-blockchain/join-testnet-manual'
           ]
         },
+        'validator/validator-manual',
+        'validator/validator-rejoin'
+      ]
+    },
+    {
+      type: 'category', 
+      label: 'Providers 📡',
+      collapsible: true,
+      collapsed: true,
+      link: {
+        type: 'doc',
+        id: 'provider/provider'
+      },
+      items: [
         {
-          type: 'category', 
-          label: 'Be a Validator',
+          type: 'category',
+          label: 'Lavavisor',
           collapsible: true,
           collapsed: true,
+          description: 'lavavisor',
           link: {
             type: 'doc',
-            id: 'validator'
+            id: 'provider/lavavisor/lavavisor',
           },
           items: [
-            'validator/validator-manual',
-            'validator/validator-rejoin'
+            'provider/lavavisor/lavavisor-wrap',
+            'provider/lavavisor/lavavisor-services',
           ]
-        },    
-        {
-          type: 'category', 
-          label: 'Be a Provider',
-          collapsible: true,
-          collapsed: true,
-          link: {
-            type: 'doc',
-            id: 'provider'
-          },
-          items: [
-            {
-              type: 'category',
-              label: 'Lavavisor',
-              collapsible: true,
-              collapsed: true,
-              description: 'lavavisor',
-              link: {
-                type: 'doc',
-                id: 'provider/lavavisor/lavavisor',
-              },
-              items: [
-                'provider/lavavisor/lavavisor-wrap',
-                'provider/lavavisor/lavavisor-services',
-              ]
-            },
-            'provider/provider-setup',
-            'provider/provider-features',
-            'provider/provider-tls',
-            'provider/provider-improve-service'
-          ]
-        },    
+        },
+        'provider/provider-setup',
+        'provider/provider-features',
+        'provider/provider-tls',
+        'provider/provider-improve-service'
+      ]
+    },
+    {
+      type: 'category', 
+      label: 'Lava Protocol 🌋',
+      collapsible: true,
+      collapsed: true,
+      link: {
+        type: 'doc',
+        id: 'power-lava-intro'
+      },
+      items: [
+        'lava-blockchain/install-lava',
+        'lava-blockchain/hardware-requirements',     
         'lava-blockchain/wallet',
         'lava-blockchain/faucet',
         'lava-blockchain/subscriptions',
         'lava-blockchain/badge-server'
       ]
     },
+    {
+      type: 'category', 
+      label: 'Get RPC / Web3 APIs ⚡️',
+      collapsible: true,
+      collapsed: true,
+      link: {
+        type: 'doc',
+        id: 'access-intro'
+      },
+      items: [
+        'public-rpc',
+      ],
+    },
     `tutorials`,
-    'explorers',
     {
       type: 'link',
       label: 'Contribute to Docs',
